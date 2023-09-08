c1aa63a6be01c621bf05d35eb61179623f7c0c8e
glmartinez-prog <gl.martinez@uniandes.edu.co>
2023-09-07 21:25:12 -0500

    Second commit

--- src/uniandes/dpoo/taller2/Modelo/Restaurante.java
+++ src/uniandes/dpoo/taller2/Modelo/Restaurante.java
@@ -6,38 +6,38 @@ import java.util.ArrayList;
 public class Restaurante {
 
 	public Restaurante() {
-		
+
 	}
-	
+
 	public void iniciarPedido(String nombreCliente, String direccionCliente) {
-		
+
 	}
-	
+
 	public void cerrarYGuardarPedido() {
-		
+
 	}
-	
+
 	public Pedido getPedidoEnCurso() {
 		return null;
 	}
-	
+
 	public ArrayList<Producto> getMenuBase() {
 		return null;
 	}
-	
+
 	public void cargarInformacionRestaurante(File archivoIngredientes, File archivoMenu, File archivoCombos) {
-		
+
 	}
-	
+
 	private void cargarIngredientes(File archivoIngredientes) {
-		
+
 	}
-	
+
 	private void cargarMenu(File archivoMenu) {
-		
+
 	}
-	
+
 	private void cargarCombos(File archivoCombos) {
-		
+
 	}
 }
